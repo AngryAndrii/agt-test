4fd2e4310849ace4b43a0b2accd3ff4cfcc22777
Andrii Chychkan <mywork1728@gmail.com>
2024-10-11 22:24:50 +0300

    Develop (#2)
    * refactored mainjs
    
    * added drag and drop
    
    ---------
    
    Co-authored-by: Andrii Chychkan <mywork1728@gmail>

---
+++ js/helpers.js
@@ -0,0 +1,13 @@
+export const rainbowColor = [
+  '#dd0000',
+  '#fe622b',
+  '#fef701',
+  '#03bd00',
+  '#019cfe',
+  '#000084',
+  '#2d009c',
+];
+
+export function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 import { fetchData } from './fetch.js';
+import { rainbowColor, randomInteger } from './helpers.js';
 
 const mainButton = document.querySelector('#main-button');
 const mainHeader = document.querySelector('#main-header');
 const headerDescr = document.querySelector('#header-descr');
-
-const rainbowColor = ['#dd0000', '#fe622b', '#fef701', '#03bd00', '#019cfe', '#000084', '#2d009c'];
-
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 const changeColor = () => {
   const num = randomInteger(0, 6);
